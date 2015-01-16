b30059fdb741cb581346006b24af7f5e8d2e2e4a
Eric Yu <eric.rongmu@gmail.com>
2015-01-16 21:46:43 +0900

    [utils] use a class to split text files

--- buncuts/utils.py
+++ buncuts/utils.py
@@ -5,15 +5,17 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-import sys
 import io
 import re
 
 default_delimeter = set("。！？▲")
 default_quote_dict = {"「": "」", "『": "』"}
 
+default_input_enc = 'sjis'
+default_output_enc = default_input_enc
 
-class QuoteChecker:
+
+class _QuoteChecker:
     """Quote checker.
 
     The instance is expected to be used in an iteration,
@@ -47,21 +49,120 @@ class QuoteChecker:
             return False
 
 
+class TextSplitter:
+    def __init__(self,
+                 input_list,
+                 output_path,
+                 input_enc=default_input_enc,
+                 output_enc=default_output_enc,
+                 output_newline=None,
+                 output_is_dir=False,
+                 output_append=False,
+                 delimiters=default_delimeter,
+                 quote_dict=default_quote_dict,
+                 check_quote=True):
+        self.input_list = input_list
+        self.input_enc = input_enc
+
+        self.output_path = output_path
+        self.output_enc = output_enc
+        self.output_newline = output_newline
+        self.output_is_dir = output_is_dir
+        if output_append:
+            self.write_mode = 'a'
+        else:
+            self.write_mode = 'w'
+
+        self.delimiters = delimiters
+        self.quote_dict = quote_dict
+        self.check_quote = check_quote
+
+    def _split_single_file(self, input_path, output_file):
+        input_file = io.open(input_path,
+                             mode='r',
+                             encoding=self.input_enc)
+        try:
+            for line in input_file:
+                line = re.sub(r"^[ 　]+|[ 　\n]+$", "", line)
+
+                if line == '':
+                    continue
+
+                line_splitted = split_line(line,
+                                           self.delimiters,
+                                           self.quote_dict,
+                                           self.check_quote)
+                output_file.write(line_splitted)
+        finally:
+            input_file.close()
+
+    def _output_to_dir(self):
+        for file_path in self.input_list:
+            if self.output_newline is None:
+                output_newline = _get_newline(file_path,
+                                              self.input_enc)
+            else:
+                output_newline = self.output_newline
+
+            output_path = os.path.join(self.output_path,
+                                       os.path.basename(file_path))
+            output_file = io.open(output_path,
+                                  mode=self.write_mode,
+                                  encoding=self.output_enc,
+                                  newline=output_newline)
+
+            try:
+                self._split_single_file(file_path, output_file)
+            finally:
+                output_file.close()
+
+    def _output_to_file(self):
+        if self.output_newline is None:
+            output_newline = _get_newline(self.input_list[0],
+                                          self.input_enc)
+        else:
+            output_newline = self.output_newline
+
+        output_file = io.open(self.output_path,
+                              mode=self.write_mode,
+                              encoding=self.output_enc,
+                              newline=output_newline)
+
+        try:
+            for file_path in self.input_list:
+                self._split_single_file(file_path, output_file)
+        finally:
+            output_file.close()
+
+    def split(self):
+        if self.output_is_dir:
+            self._output_to_dir()
+        else:
+            self._output_to_file()
+
+
+def _get_newline(file_path, enc):
+    newline = None
+
+    with io.open(file_path, 'r', encoding=enc) as f:
+        f.readline()
+        newline = f.newlines
+
+    return newline
+
+
 def split_line(line,
-               sentence_delim=default_delimeter,
-               check_quote=True,
-               quote_dict=default_quote_dict):
+               delimiters=default_delimeter,
+               quote_dict=default_quote_dict,
+               check_quote=True):
     """Split a line into lines of sentences.
 
     Returns:
         The resulted string
     """
     result = ""
-    qc = QuoteChecker(quote_dict)
-
-    # the chars that after a sentence delimiter
-    # that should prevent insertion of newlines.
-    prevent_newline = sentence_delim | set('\n')
+    newline_preventers = delimiters | {'\n'}
+    qc = _QuoteChecker(quote_dict)
 
     line = line.rstrip('\n')
     length = len(line)
@@ -77,8 +178,8 @@ def split_line(line,
                 continue
 
         # Additionaly, append a newline after a sentence delimeter.
-        if char in sentence_delim:
-            if ((i <= length - 2 and line[i+1] not in prevent_newline)
+        if char in delimiters:
+            if ((i <= length - 2 and line[i+1] not in newline_preventers)
                     or i == length - 1):
                 result = ''.join((result, '\n'))
 
@@ -87,139 +188,16 @@ def split_line(line,
             and qc.first_append_index == 0
             and qc.first_pop_index == length - 1):
         reparsed = split_line(result[1:-1],
-                              sentence_delim,
-                              check_quote,
-                              quote_dict)
-        # rstrip to avoid \n between last sentence delimeter
-        # and last quotation mark.
+                              delimiters,
+                              quote_dict,
+                              check_quote)
+        # rstrip to avoid \n before the last quotation mark.
         reparsed = reparsed.rstrip('\n')
-        return ''.join((result[0],
-                        reparsed,
-                        result[-1]))
-    else:
-        return result
+        result = ''.join((result[0],
+                          reparsed,
+                          result[-1]))
 
+    if result[-1] != '\n':
+        result = ''.join((result, '\n'))
 
-def process_single_file(input=sys.stdin,
-                        input_enc='sjis',
-                        output=sys.stdout,
-                        output_enc='sjis',
-                        output_newline=None,
-                        append=False,
-                        is_dir=False,
-                        sentence_delim=default_delimeter,
-                        quote_dict=default_quote_dict,
-                        limit=float('inf'),
-                        echo=False):
-    """Perform line breaks on one file.
-
-    Call ``split_line()`` for each line in the input file.
-
-    Args:
-        input: Path of the input file.
-        input_enc: Character encoding of the input files.
-            Defauts to Shift-JIS.
-        output: Path of output destination.
-            Defauts to stdout.
-        output_enc: Character encoding of the output file(s).
-            Defauts to Shift-JIS.
-        output_newline: The newline formart of the output file(s).
-        append: Whether append to output or not.
-        is_dir: Whether the output is a directory or a regular file.
-        sentence_delim: A set of sentence delimeters.
-        quote_dict: A dict that maps opening quote marks
-            to its closing counterpart.
-        limit: The limit for maximum amout of sentences
-            that should be extracted.
-        echo: Whether echo the output or not.
-    """
-    count = 0
-
-    if append:
-        mode = 'a'
-    else:
-        mode = 'w'
-
-    if input is not sys.stdin:
-        input_file = io.open(input,
-                             mode='r',
-                             encoding=input_enc)
-
-    else:
-        input_file = input
-
-    # determine the newline format.
-    # io.TextIOBase.newlines only indicates the newlines translated so far.
-    # so you have to read one line in order to determine the newline.
-    input_file.readline()
-    input_newline = input_file.newlines
-    input_file.seek(0)
-    if output_newline is None:
-        output_newline = input_newline
-
-    if output is not sys.stdout:
-        if is_dir:
-            path = os.path.join(output, os.path.basename(input))
-            output_file = io.open(path,
-                                  mode=mode,
-                                  encoding=output_enc,
-                                  newline=output_newline)
-
-        else:
-            output_file = io.open(output,
-                                  mode=mode,
-                                  encoding=output_enc,
-                                  newline=output_newline)
-    else:
-        output_file = output
-
-    for line in input_file:
-        # strip half/full width spaces
-        # strip() somehow don't work very well.
-        # use re instead.
-        line = re.sub(r"^[ 　]+|[ 　\n]+$", "", line)
-
-        if line == '':
-            continue
-
-        line_splitted = split_line(line,
-                                   sentence_delim,
-                                   quote_dict)
-
-        if line_splitted[-1] != '\n':
-            line_splitted = ''.join((line_splitted, '\n'))
-
-        count += line_splitted.count('\n')
-        output_file.write(line_splitted)
-
-    # close files
-    if input is not sys.stdin:
-        input_file.close()
-
-    if output is not sys.stdout:
-        output_file.close()
-
-
-def split_sentences(input_list,
-                    input_enc='sjis',
-                    output=sys.stdout,
-                    output_enc='sjis',
-                    append=False,
-                    is_dir=False,
-                    sentence_delim=default_delimeter,
-                    quote_dict=default_quote_dict,
-                    limit=float('inf'),
-                    echo=False):
-    """Split the text from input files into sentences.
-
-    Call ``process_single_file()`` for each file in the input file list.
-    """
-    for f in input_list:
-        process_single_file(input=f,
-                            output=output,
-                            append=append,
-                            is_dir=is_dir,
-                            sentence_delim=sentence_delim,
-                            quote_dict=quote_dict,
-                            limit=limit,
-                            echo=echo)
+    return result
